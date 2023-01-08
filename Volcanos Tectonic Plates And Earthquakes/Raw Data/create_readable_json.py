5cd0badb5c7998f6a43cd4127f4916547e5a62c0
Simon Abhijet Biswas <96303186+simon619@users.noreply.github.com>
2023-01-08 14:02:55 +0600

    Update create_readable_json.py

--- Volcanos Tectonic Plates And Earthquakes/Raw Data/create_readable_json.py
+++ Volcanos Tectonic Plates And Earthquakes/Raw Data/create_readable_json.py
@@ -3,7 +3,7 @@ import json
 with open('volcano_location_raw_json_data.json') as f:
     purified_volcano_data = json.load(f)
 
-with open('purified_data.json', 'w') as file:
+with open('volcano_location_data.json', 'w') as file:
     json.dump(purified_volcano_data, file, indent=2)
 #
 
